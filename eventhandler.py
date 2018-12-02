7f19b481cc848b2d17bad968c27a5f9d1880c740
iBug <7273074+iBug@users.noreply.github.com>
2018-12-02 10:58:07 +0800

    Handle post fetch failure

--- eventhandler.py
+++ eventhandler.py
@@ -9,5 +9,8 @@ def on_new_post(j):
     log('debug', "New post ID: {}".format(j['id']))
 
     post = seapi.get_post(core.config.site, int(j['id']), "question")
+    if not post:
+        log('debug', "Fetched nothing for post {}".format(j['id']))
+        return
     log('debug', "Fetched post: [{}] {}".format(post.id, post.title))
     core.worker.scanner.enqueue(post)
