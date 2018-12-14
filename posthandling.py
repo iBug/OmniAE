58ee696faa6ce03520a1db76861459bf6a5fb6fd
iBug <7273074+iBug@users.noreply.github.com>
2018-12-14 09:24:23 +0800

    increase grace to 2d

--- posthandling.py
+++ posthandling.py
@@ -17,7 +17,7 @@ class PostHandler:
 
         post = result.post
         if result.scanner.name == "development question":
-            if post.creation_date < datetime.now().timestamp() - 300:
+            if post.creation_date < datetime.now().timestamp() - 172800:  # 2 days
                 log('attention', "Post too old ({}), ignored, score={}".format(
                     datetime.fromtimestamp(post.creation_date).isoformat(),
                     result.score))
