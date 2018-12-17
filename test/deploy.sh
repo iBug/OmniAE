313a7739b00423c3854dace9e2a988a062db3096
iBug <7273074+iBug@users.noreply.github.com>
2018-12-17 11:52:16 +0800

    huh?

--- test/deploy.sh
+++ test/deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-e_info() { echo -e "\e[36;1m[Info]\e[0m $*" >&2 }
-e_warning() { echo -e "\e[33;1m[Warning]\e[0m $*" >&2 }
-e_error() { echo -e "\e[31;1m[Error]\e[0m $*" >&2 }
-e_success() { echo -e "\e[32;1m[Success]\e[0m $*" >&2 }
+e_info() { echo -e "\e[36;1m[Info]\e[0m $*" >&2; }
+e_warning() { echo -e "\e[33;1m[Warning]\e[0m $*" >&2; }
+e_error() { echo -e "\e[31;1m[Error]\e[0m $*" >&2; }
+e_success() { echo -e "\e[32;1m[Success]\e[0m $*" >&2; }
 
 if [ -z "$GH_TOKEN" ]; then
   e_error "No GitHub token is set, cannot deploy."
