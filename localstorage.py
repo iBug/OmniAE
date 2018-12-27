b6a172765a06e959f2bea60bb06681846488d437
iBug <7273074+iBug@users.noreply.github.com>
2018-12-27 23:32:19 +0800

    partial DB handler

--- localstorage.py
+++ localstorage.py
@@ -3,7 +3,7 @@ import threading
 
 
 class LocalStorage:
-    def __init__(self, filename):
+    def __init__(self, filename: str):
         self.dbs = {}
         self.filename = filename
 
@@ -13,10 +13,10 @@ class LocalStorage:
             self.dbs[thread_id] = sqlite3.connect(self.filename)
         return self.dbs[thread_id]
 
-    def __call__(self, query, params=(), **kwargs):
+    def __call__(self, query: str, params=(), **kwargs):
         return self.execute(query, params, **kwargs)
 
-    def execute(self, query, params=(), **kwargs):
+    def execute(self, query: str, params=(), **kwargs):
         thread_id = threading.get_ident()
         try:
             db = self.dbs[thread_id]
@@ -24,7 +24,19 @@ class LocalStorage:
             raise
         return db.execute(query, tuple(params), **kwargs)
 
+    def table_exists(self, table="sqlite_master"):
+        return bool(self.execute("SELECT name FROM sqlite_master WHERE type='table' AND name=?", [table]))
+
 
 class PostStorage(LocalStorage):
-    def __init__(self):
-        raise NotImplementedError
+    def __init__(self, filename):
+        self.ls = LocalStorage(filename)
+
+    def open(self):
+        self.ls.open()
+
+    def initialize(self):
+        pass
+
+    def add(self, post):
+        pass
