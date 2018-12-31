0fcfe411b5068181548abbb68bdedd1dd2c9c8cc
iBug <7273074+iBug@users.noreply.github.com>
2018-12-31 15:34:46 +0800

    still commit DB

--- localstorage.py
+++ localstorage.py
@@ -14,37 +14,32 @@ class LocalStorage:
             self.dbs[thread_id] = sqlite3.connect(self.filename)
         return self.dbs[thread_id]
 
+    def get_db(self, thread_id=None):
+        thread_id = thread_id or threading.get_ident()
+        if self.auto_open:
+            return self.open()
+        elif thread_id in self.dbs:
+            return self.dbs[thread_id]
+        else:
+            return None
+
     def __call__(self, query: str, params=(), **kwargs):
         r = self.execute(query, params, **kwargs)
         self.commit()
         return r
 
     def execute(self, query: str, params=(), **kwargs):
-        thread_id = threading.get_ident()
-        try:
-            if self.auto_open:
-                db = self.open()
-            else:
-                db = self.dbs[thread_id]
-        except KeyError:
-            raise
-        return db.execute(query, tuple(params), **kwargs)
+        return self.get_db().execute(query, tuple(params), **kwargs)
 
     def commit(self):
-        thread_id = threading.get_ident()
-        try:
-            if self.auto_open:
-                db = self.open()
-            else:
-                db = self.dbs[thread_id]
-        except KeyError:
-            raise
-        db.commit()
+        self.get_db().commit()
 
     def create_table(self, table, schema):
         if self.table_exists(table):
             return False
-        return self.execute("CREATE TABLE {} ({})".format(table, ", ".join(schema)))
+        r = self.execute("CREATE TABLE {} ({})".format(table, ", ".join(schema)))
+        self.commit()
+        return r
 
     def table_exists(self, table="sqlite_master"):
         return bool(self.execute("SELECT name FROM sqlite_master WHERE type='table' AND name=?;", [table]))
@@ -65,4 +60,4 @@ class PostStorage(LocalStorage):
         self.ls.create_table(self.table, ["id INTEGER", "url TEXT PRIMARY KEY", "type TEXT", "site TEXT", "owner_url TEXT", "owner_name TEXT", "owner_rep INTEGER", "title TEXT", "body TEXT", "raw_body TEXT", "score INTEGER", "upvote INTEGER", "downvote INTEGER", "question_id INTEGER", "creation_date INTEGER", "last_edit_date INTEGER"])
 
     def add(self, post):
-        return self.ls.execute("INSERT INTO {} VALUES ({})".format(self.table, ", ".join("?" * 16)), params=post.to_list())
+        return self.ls("INSERT INTO {} VALUES ({})".format(self.table, ", ".join("?" * 16)), params=post.to_list())
