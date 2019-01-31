573d6c52bf2a23e8fd0a0084c576f7090476553a
iBug <7273074+iBug@users.noreply.github.com>
2019-01-31 14:41:04 +0800

    less verbose

--- sews.py
+++ sews.py
@@ -18,7 +18,8 @@ class StackExchangeWebSocket:
             raise ValueError("WebSocket is already open")
 
         for tries in range(1, 1 + max_tries, 1):
-            log('debug', "Trying to open websocket (Attempt {})".format(tries))
+            if trieds > 1:
+                log('debug', "Trying to open websocket (Attempt {})".format(tries))
             try:
                 self.ws = websocket.create_connection(StackExchangeWebSocket.WS_URL)
                 log('debug', "Successfully opened websocket")
