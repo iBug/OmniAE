c32caab414ae34482150e295d89cc01e83a3be36
iBug <7273074+iBug@users.noreply.github.com>
2019-02-06 13:23:56 +0800

    fix daemon.py

--- daemon.py
+++ daemon.py
@@ -11,7 +11,7 @@ try:
         cmd = [PYTHON, "main.py"]
         cp = subprocess.run(cmd)
 
-        if cp.return_code != 0:
+        if cp.returncode != 0:
             print("Program exited unexpectedly, sleeping before restart")
             time.sleep(3)
 except (SystemExit, KeyboardInterrupt):
